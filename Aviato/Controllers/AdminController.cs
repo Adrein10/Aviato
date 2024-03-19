5b60ce666226b6f0b70b2fda74eeb155724d8deb
asp <asp@aptech.com>
2024-03-19 16:37:54 +0500

    added admin panel

---
+++ Aviato/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Aviato.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
